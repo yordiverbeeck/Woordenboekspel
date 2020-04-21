2907d12485eb066210784aabd3713312b05ecfb1
Yordi Verbeeck <hello@yordi.net>
2020-04-22 01:14:11 +0200

    almost finished views WIP

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
 	var currentRoom = "27cPsHOUnJzK54esCE1Y";
-	var allUsers=[];
-	var currentWord="";
+	var allUsers = [];
+	var currentWord = "";
+	var me = "";//current user
+
+	
 
 	//listener
 	var wordsListener;
@@ -15,7 +18,7 @@ $(document).ready(function() {
 
 	//listen to all user updates & scores and shit	
 	db.collection("rooms").doc(currentRoom)
-	.collection("users").where("allowed","==",true)
+	.collection("users").orderBy("createdDate","desc").where("allowed","==",true)
 	.onSnapshot(function(snap) {
     	var userHtml="";
     	allUsers=[];
@@ -24,29 +27,42 @@ $(document).ready(function() {
         		"username":doc.data().username,
         		"punten":doc.data().punten,
         	};
-        	userHtml+=`<div>
+        	userHtml+=`<div data-userid="${doc.id}">
                         <h3>${doc.data().username}</h3><h4 class="text-muted score">${doc.data().punten}</h4>
                     </div>`;
         });
         $(".deelnemers").html(userHtml);
     });
 
-    //listen to all words	
+    //listen to all words, select latest	
 	db.collection("rooms").doc(currentRoom)
 	.collection("woorden").orderBy("createdDate","desc").limit(1)
 	.onSnapshot(function(snap) {
     	var userHtml="";
         snap.forEach(function(doc) {
-       		$("#word").text(doc.data().woord);
-       	
+       		
+       		//if it changed, listen to the current word
 	       	if(currentWord != doc.data().woord){
-	     	alert(doc.id)
+       			$("#word,span.theword").text(doc.data().woord);
+
+       			$(".deelnemers > div").removeClass('wordOwner');
+       			$(".deelnemers > div[data-userid='"+doc.data().wordOwner+"']").addClass('wordOwner');
+
 	       		db.collection("rooms").doc(currentRoom)
 				.collection("woorden").doc(doc.id).collection("submissions")
 				.onSnapshot(function(submissions) {
 			    	var wordHtml="";
 			        submissions.forEach(function(submission) {
-			       		wordHtml+=`<li>${submission.data().uitleg}</li>`;
+			    		var data=submission.data();
+			       		wordHtml+=`<li class="${data.voted ? (data.voted.includes(me) ? "selected":""):"" }">${data.uitleg} `;
+	       				if(data.voted){
+				       		if(data.voted.length == 1){
+								wordHtml+=`<span class="badge badge-primary"> (1 Stem)</span>`;
+				       		}else if(data.voted.length > 1){
+								wordHtml+=`<span class="badge badge-primary">(${data.voted.length} Stemmen)</span>`;
+				       		}
+				       	}
+			       		wordHtml+=`</li>`;
 			       	});
 			       	$("#wordExplanation").html(wordHtml);
 			    });
@@ -54,6 +70,45 @@ $(document).ready(function() {
         });
 
     });
+
+    function init(){
+    	$('[data-toggle="tooltip"]').tooltip();
+ 		
+ 		//general listeners
+	    $(document).on('keyup', '#betekenisinput', function(event) {
+	    	event.preventDefault();
+	    	 if($(this).val().length !=0)
+	            $('#submitBetekenis').removeClass('disabled');            
+	        else
+	            $('#submitBetekenis').addClass('disabled');
+
+	    }).on('click', '#submitBetekenis', function(event) {
+	    	event.preventDefault();
+	    	if(!$(this).hasClass('disabled') && $("#betekenisinput").val().length !=0){
+				console.log("oi");
+	    	}
+	    }).on('keyup', '#woordinput', function(event) {
+	    	event.preventDefault();
+	    	 if($(this).val().length !=0)
+	            $('#submitWoord').removeClass('disabled');            
+	        else
+	            $('#submitWoord').addClass('disabled');
+
+	    }).on('click', '#submitWoord', function(event) {
+	    	event.preventDefault();
+	    	if(!$(this).hasClass('disabled') && $("#woordinput").val().length !=0){
+				console.log("oi");
+	    	}
+	    }).on('click', '#resetWord > a', function(event) {
+	    	event.preventDefault();
+	    	/* Act on the event */
+	    });;
+
+    }
+    init();
+
+    $(".wordOwnerOnly").css('display', 'block');
+	   
 
 
 	//call when a user has sent a word
